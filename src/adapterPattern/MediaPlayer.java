12fa1bde88e6e2eb14dfea42cef6df51ccf5e640
Direct <Direct@Direct-PC>
2019-07-08 18:55:31 +0800

    adapterPattern

---
+++ src/adapterPattern/MediaPlayer.java
@@ -0,0 +1,5 @@
+package adapterPattern;
+//	为媒体播放器和更高级的媒体播放器常见接口
+public interface MediaPlayer {
+	public void play(String audioType,String fileName);
+}
---
+++ src/adapterPattern/Mp4Player.java
@@ -0,0 +1,15 @@
+package adapterPattern;
+
+public class Mp4Player implements AdvancedMediaPlayer{
+
+	@Override
+	public void playVlc(String fileName) {
+//		什么也不做
+	}
+//	构造playMp4方法
+	@Override
+	public void playMp4(String fileName) {
+		System.out.println("Playing mp4 file.Name"+fileName);
+	}
+
+}
